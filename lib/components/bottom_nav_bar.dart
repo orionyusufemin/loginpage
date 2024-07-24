2de11faba0abd2b367f909e1fc84b629d8ea3ab2
orionyusufemin <yusufemin@orionpos.com>
2024-07-24 13:25:03 +0300

    buttom nav bar eklendi

---
+++ lib/components/bottom_nav_bar.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class MyBottomNavBar extends StatelessWidget {
+  void Function(int)? onTabChange; //onemlı
+  MyBottomNavBar({super.key, required this.onTabChange}); //onemlı
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: GNav(
+        onTabChange: (value) => onTabChange!(value), //onemlı
+        padding: EdgeInsets.all(25),
+        tabBorderRadius: 5,
+        color: Colors.grey,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        activeColor: Colors.black,
+        tabActiveBorder: Border.all(color: Colors.grey.shade700),
+        tabBackgroundColor: Colors.white,
+        tabs: [
+          //onemlı
+          GButton(
+            icon: Icons.home,
+            text: "Shop",
+          ),
+          GButton(
+            icon: Icons.shopping_cart_rounded,
+            text: "Cart",
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/feed_page.dart
+++ lib/pages/feed_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shoppingapp/components/bottom_nav_bar.dart';
+import 'package:shoppingapp/pages/profile_page.dart';
+import 'package:shoppingapp/pages/shop_page.dart';
 
 class FeedPage extends StatefulWidget {
   const FeedPage({super.key});
@@ -8,10 +11,34 @@ class FeedPage extends StatefulWidget {
 }
 
 class _FeedPageState extends State<FeedPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      
-    );
+  int _selectedIndex = 0; 
+
+  void navigateBottomBar(int index) {
+    if (mounted) {
+      setState(() {
+        _selectedIndex = index;
+      });
+    }
+    
   }
+
+  late List<Widget> _pages;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _pages = [
+      
+      ShopPage(),
+      ProfilePage()
+    ];
+    
+  }
+  @override
+    Widget build(BuildContext context) {
+      return Scaffold(body: _pages[_selectedIndex],
+      bottomNavigationBar: MyBottomNavBar(onTabChange: (index) => navigateBottomBar(index)),
+      );
+    }
 }
