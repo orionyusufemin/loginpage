af9b3b3cc685a427b8ffa7d04edf432372f7a516
orionyusufemin <yusufemin@orionpos.com>
2024-07-26 14:08:20 +0300

    two graphs added

--- lib/models/my_bar_chart.dart
+++ lib/models/my_bar_chart.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last
+
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 
@@ -9,101 +11,141 @@ class MyBarChart extends StatefulWidget {
 }
 
 class _MyBarChartState extends State<MyBarChart> {
-  final List<String> months = [
-    'Mayis',
-    'Haziran',
-    'Temmuz',
-    'Ağustos',
-    'Eylül',
-    'Ekim'
-  ];
-  final List<double> values = [23983, 12863, 98536, 39835, 41238, 76921];
+  bool isSwitched = false;
+  List<String> monthNames = ["May", "Haz", "Temm", "Agu", "Eyl", "Ekm"];
+  List<int> saleCount = [23423, 89763, 12974, 65384, 92648, 42638];
+  List<int> saleCount1 = [3423, 12331, 12174, 6575, 1213, 6787];
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
-      child: BarChart(
-        BarChartData(
-          alignment: BarChartAlignment.spaceBetween,
-          maxY: 100000,
-          barTouchData: BarTouchData(enabled: false),
-          titlesData: FlTitlesData(
-            show: true,
-            bottomTitles: AxisTitles(
-              sideTitles: SideTitles(
-                showTitles: true,
-                getTitlesWidget: (value, meta) {
-                  const style = TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 10,
-                  );
-                  Widget text;
-                  switch (value.toInt()) {
-                    case 0:
-                      text = const Text('Mayis', style: style);
-                      break;
-                    case 1:
-                      text = const Text('Haziran', style: style);
-                      break;
-                    case 2:
-                      text = const Text('Temmuz', style: style);
-                      break;
-                    case 3:
-                      text = const Text('Ağustos', style: style);
-                      break;
-                    case 4:
-                      text = const Text('Eylül', style: style);
-                      break;
-                    case 5:
-                      text = const Text('Ekim', style: style);
-                      break;
-                    default:
-                      text = const Text('', style: style);
-                      break;
-                  }
-                  return SideTitleWidget(child: text, axisSide: meta.axisSide);
-                },
-                reservedSize: 40,
-              ),
-            ),
-            leftTitles: AxisTitles(
-              sideTitles: SideTitles(
-                showTitles: true,
-                getTitlesWidget: (value, meta) {
-                  return Text(
-                    value.toInt().toString(),
-                    style: const TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 14,
-                    ),
-                  );
-                },
-                reservedSize: 40,
-                interval: 20000,
-              ),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "Monthly Sales",
+              style: TextStyle(fontSize: 48),
             ),
           ),
-          borderData: FlBorderData(
-            show: false,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("Graph view"),
+              Switch(
+                value: isSwitched,
+                onChanged: (value) {
+                  setState(() {
+                    isSwitched = value;
+                  });
+                },
+              ),
+              Text("Table view")
+            ],
           ),
-          barGroups: values.asMap().entries.map((entry) {
-            return BarChartGroupData(
-              x: entry.key,
-              barRods: [
-                BarChartRodData(
-                  toY: entry.value,
-                  color: Colors.blue,
-                  width: 16,
-                  borderRadius: BorderRadius.zero,
-                )
-              ],
-            );
-          }).toList(),
-        ),
+          Container(
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(color: Colors.black54),
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 0.5,
+            child: !isSwitched
+                ? BarChart(BarChartData(
+                    maxY: 100000,
+                    backgroundColor: Colors.black26,
+                    gridData: FlGridData(show: false),
+                    titlesData: FlTitlesData(
+                        rightTitles: AxisTitles(
+                            sideTitles: SideTitles(showTitles: false)),
+                        topTitles: AxisTitles(
+                            sideTitles: SideTitles(showTitles: false)),
+                        leftTitles: AxisTitles(
+                            sideTitles: SideTitles(
+                          showTitles: true,
+                          reservedSize: 50,
+                          getTitlesWidget: (value, meta) {
+                            return SideTitleWidget(
+                                child: Text(
+                                  value.toInt().toString(),
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                axisSide: meta.axisSide);
+                          },
+                        )),
+                        bottomTitles: AxisTitles(
+                            sideTitles: SideTitles(
+                          showTitles: true,
+                          getTitlesWidget: (value, meta) {
+                            int index = value.toInt();
+                            return SideTitleWidget(
+                                child: Text(
+                                  monthNames[index],
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                axisSide: meta.axisSide);
+                          },
+                        ))),
+                    alignment: BarChartAlignment.spaceEvenly,
+                    barGroups: List.generate(
+                      monthNames.length,
+                      (index) {
+                        return BarChartGroupData(x: index, barRods: [
+                          BarChartRodData(
+                              toY: saleCount[index].toDouble(), width: 15)
+                        ]);
+                      },
+                    )))
+                : Text("sasdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "Total Income",
+              style: TextStyle(fontSize: 48),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height * 0.5,
+            decoration: BoxDecoration(color: Colors.black54),
+            child: BarChart(BarChartData(
+                maxY: 200000,
+                gridData: FlGridData(show: false),
+                backgroundColor: Colors.black26,
+                titlesData: FlTitlesData(
+                    leftTitles: AxisTitles(
+                        sideTitles: SideTitles(
+                      reservedSize: 58,
+                      showTitles: true,
+                      getTitlesWidget: (value, meta) {
+                        return Text(
+                          value.toString(),
+                          style: TextStyle(),
+                        );
+                      },
+                    )),
+                    bottomTitles: AxisTitles(
+                        sideTitles: SideTitles(
+                      showTitles: true,
+                      getTitlesWidget: (value, meta) {
+                        int index = value.toInt();
+                        return Text(
+                          monthNames[index].toString(),
+                          style: TextStyle(color: Colors.white),
+                        );
+                      },
+                    ))),
+                barGroups: List.generate(
+                  saleCount.length,
+                  (index) {
+                    return BarChartGroupData(x: index, barRods: [
+                      BarChartRodData(toY: saleCount1[index] * 14, width: 12)
+                    ]);
+                  },
+                ))),
+          ),
+          
+        ],
       ),
     );
   }
